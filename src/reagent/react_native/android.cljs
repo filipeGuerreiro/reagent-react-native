9e50f7d8c369a0db485ea401d046fbc423ba6118
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2020-11-14 22:13:15 +0900

    Add remaining components

--- src/reagent/react_native/android.cljs
+++ src/reagent/react_native/android.cljs
@@ -4,5 +4,8 @@
 
 (def date-picker (r/adapt-react-class rn/DatePickerAndroid))
 (def drawer-layout (r/adapt-react-class rn/DrawerLayoutAndroid))
+(def native-dialog-manager (r/adapt-react-class rn/NativeDialogManagerAndroid))
+(def permissions (r/adapt-react-class rn/PermissionsAndroid))
 (def progress-bar (r/adapt-react-class rn/ProgressBarAndroid))
+(def toast (r/adapt-react-class rn/ToastAndroid))
 (def touchable-native-feedback (r/adapt-react-class rn/TouchableNativeFeedback))
